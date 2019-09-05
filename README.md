d19a08b33eb72f1dfd2dcfdbeafd169bfafaeee5
weng.yitong <weng.yitong@linecorp.com>
2019-09-05 16:49:47 +0900

    Initialize app with sample codes

---
+++ README.md
@@ -0,0 +1,7 @@
+# perseus_chat_client
+
+A Flutter project for Perseus Chat Client.
+
+## Getting Started
+
+See Flutter documents: https://flutter.dev/docs/get-started/install
---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(PerseusChatApp());
+
+class PerseusChatApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Perseus Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: HomePage(title: 'Perseus Chat Home Page'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
